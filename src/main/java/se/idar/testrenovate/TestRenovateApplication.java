749aeb5d742b5a9f181306419be5b84fc1b32285
Idar Österlind <idar.osterlind@nordnet.se>
2019-02-04 13:14:00 +0100

    init

---
+++ src/main/java/se/idar/testrenovate/TestRenovateApplication.java
@@ -0,0 +1,14 @@
+package se.idar.testrenovate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRenovateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRenovateApplication.class, args);
+	}
+
+}
+
